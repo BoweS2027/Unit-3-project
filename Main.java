de4cff8bdf4f46caf8aa2530c1d5e72d69f9bd0a
BoweS2027 <bowe.siegelson2027@gbwl.org>
2026-01-28 18:10:01 +0000

    almost

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
-      physicsTrip trip = new physicsTrip();
-      physicsTrip trip2 = new physicsTrip("Danny", "land", 2, true);
-      physicsTrip trip3 = new physicsTrip("Mathew", "your moms house", 10000, true);
+      Scanner scan = new Scanner(System.in);
+      String command = scan.next();
+      String command2 = scan.next();;
+      int command3 = scan.nextInt();;
+      String command4 = scan.next();;
+      boolean command5 = false;
+      if (command4 == "yes" || command4 == "y"){
+         command5=true;
+      }
+      physicsTrip trip = new physicsTrip(command,command2,command3,command5);
       System.out.println(trip);
-      System.out.println(trip2);
-      System.out.println(trip3);
    }
 }
